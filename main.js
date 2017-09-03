410d8ff97ca151aadaf6237072e0bf22cc4e03f1
Luke Palmer <lrpalmer@gmail.com>
2017-09-02 19:09:42 -0700

    More

--- main.js
+++ main.js
@@ -24,6 +24,22 @@ $$.Rational.prototype.asNumber = function() {
 };
 
 
+// Like a Grid, but is actually a bounding box (rather than a y box and an x scale)
+$$.Bounds = function(x0, y0, x1, y1) {
+    this.x0 = x0;
+    this.y0 = y0;
+    this.x1 = x1;
+    this.y1 = y1;
+};
+
+$$.Bounds.prototype.sliceX = function(percent) {
+    return [
+        new $$.Bounds(this.x0, this.y0, this.x0 + (this.x1-this.x0)*percent, this.y1),
+        new $$.Bounds(this.x0 + (this.x1-this.x0)*percent, this.y0, this.x1, this.y1)
+    ];
+};
+
+
 // Only axis-aligned transformations are allowed, because we're drawing
 // rectangles.
 $$.Grid = function(x0, y0, x1, y1) {
@@ -74,31 +90,63 @@ $$.Rhythm.prototype.draw = function(ctx, grid, xbase) {
     var x1 = grid.locateX(xbase + this.size());
     var y0 = grid.locateY(0);
     var y1 = grid.locateY(1);
-    console.log(x0, y0, x1, y1);
     ctx.fillRect(x0, y0, x1 - x0, y1 - y0);
 };
 
 
-$$.Sequence = function(numtracks) {
+$$.Sequence = function(numtracks, bounds) {
     this.tracks = [];
+    this.bounds = bounds;
     for (var i = 0; i < numtracks; i++) {
         this.tracks.push(new $$.Stack());
     }
 };
 
-$$.Sequence.prototype.draw = function(ctx, bounds) {
+$$.Sequence.prototype.draw = function(ctx) {
     for (var i = 0; i < this.tracks.length; i++) {
-        let dy = (bounds.y1 - bounds.y0)/this.tracks.length;
+        let dy = (this.bounds.y1 - this.bounds.y0)/this.tracks.length;
         this.tracks[i].draw(
             ctx, 
-            new $$.Grid(bounds.x0, 
-                        bounds.y0 + i*dy,
-                        bounds.x1 / 100,
-                        bounds.y0 + (i+1)*dy));
+            new $$.Grid(this.bounds.x0, 
+                        this.bounds.y0 + i*dy,
+                        this.bounds.x0 + (this.bounds.x1 - this.bounds.x0) / 100,
+                        this.bounds.y0 + (i+1)*dy));
 
     }
 };
 
+$$.Sequence.prototype.rowYBounds = function(row) {
+    let dy = (this.bounds.y1 - this.bounds.y0) / this.tracks.length;
+    let r = [ this.bounds.y0 + row*dy, this.bounds.y0 + (row+1)*dy ];
+    return r;
+};
+
+
+$$.Game = function(numtracks, bounds) {
+    this.bounds = bounds;
+    var slice = this.bounds.sliceX(0.75);
+    this.sequence = new $$.Sequence(numtracks, slice[0]);
+    
+
+    this.piece = new $$.Rhythm(new $$.Rational(1,1), 10);
+    this.pieceColBounds = slice[1];
+    this.pieceRow = 0;
+};
+
+$$.Game.prototype.draw = function(ctx) {
+    this.sequence.draw(ctx);
+    if (this.piece) {
+        let ybounds = this.sequence.rowYBounds(this.pieceRow);
+        this.piece.draw(ctx, new $$.Grid(this.pieceColBounds.x0, ybounds[0], this.pieceColBounds.x0 + 10, ybounds[1]), 0);
+    }
+};
+
+$$.Game.prototype.insert = function() {
+    if (this.piece) {
+        this.sequence.tracks[this.pieceRow].push(this.piece);
+        this.piece = new $$.Rhythm(new $$.Rational(1,1), 10);
+    }
+};
 
 return $$;
 
